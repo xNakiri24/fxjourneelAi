053bef18ca456bc1371e42bb6dcda8e5873fca5c
Christian Philip <christianalupit24@outlook.com>
2025-03-05 10:51:13 +0800

    Update firebase.js
    remove my firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -8,14 +8,16 @@ import { getAuth } from "firebase/auth"
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Add here you own firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyB5m5KpBw6nA3_LSSISHdja4a_EdoPrGV0",
-  authDomain: "journeel.firebaseapp.com",
-  projectId: "journeel",
-  storageBucket: "journeel.appspot.com",
-  messagingSenderId: "62004648338",
-  appId: "1:62004648338:web:5a2746a3eb84938431894b",
-  measurementId: "G-BRPF6J766V"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
